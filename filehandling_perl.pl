7b09c8d61df20dee7b298e5b1982b6f0c8a6664b
suraj upadhaya <srupadhaya15@gmail.com>
2022-09-12 23:56:21 +0530

    testing

--- filehandling_perl.pl
+++ filehandling_perl.pl
@@ -1,20 +1,70 @@
 #!/usr/bin/perl
 
-open(FHR,"<","test1.txt") or die "Nope";
+#my $start=time();
 
-#my $test=<FHR>;
- $tn = '';
- 
-for (<FHR>){
+use strict;
+use warnings 'all';
+print "-------------------------------------\n";
+print "ENTER THE RECORD YOU WANT TO SEARCH\n ";
+print "-------------------------------------\n";
+
+my $input = <STDIN>;
+chomp($input);
+
+print "--------------------\n";
+print "ENTER THE FILE NAME \n ";
+print "--------------------\n";
+
+my $file= <STDIN>;
+chomp($file);
+
+print "-------------------\n";
+
+my ($name,$Id,$code,$city);
+my  $tn = '';
+my $count=0;
+my $count2=0;
+open(FHR,"<",$file) or die $!;
+
+
+=begin comment
+while (<FHR>){
+next if /^#/;
+next if /^\n/;
+$count++;
+}
+ print "$count\n";
+=cut 
+while (<FHR>)
+{
 	next if /^#/;
 	next if /^\n/;
-	$tn .= $_;
+	$count++;
+	$tn = $_;
 	$tn =~ s/$/,/;	
-	my ($name , $Id , $code , $city) = split /,/,$_;
-	print "Name:$name\n","ID:$Id\n","Code:$code\n","City:$city\n";
-}
-close(FHR) or die "nope";
+	($name , $Id , $code , $city) = split /,/,$tn;
+	if ($input =~  /^\Q$name\E/i)
+	
+		{
+	        	print "NAME:$name\n", "CITY:$city\n","CODE:$code\n";
+			last ;
+			}
+	else  
+			{
+	  $count2++;	   			
+	  next;		
+	  	
+	  }	
+  }
 
+
+if ($count == $count2)
+{
+
+print "Record Not Present\n";
+}
+
+close(FHR) or die "$!";
 
 
 
